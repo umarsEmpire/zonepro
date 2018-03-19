50f4d854fc404c758f1896e28425211763e5f4be
Mohammed Usman <zad.designer@gmail.com>
2018-03-19 23:37:34 +0530

    added new files in zone pro

--- js/members-screen.js
+++ js/members-screen.js
@@ -20,4 +20,30 @@ $(document).ready(function () {
         }
         clickEvent = false;
     });
+<<<<<<< HEAD
+});
+
+$(document).ready(function(){
+    $('#php').LineProgressbar({
+          percentage: 99,
+          fillBackgroundColor: '#1abc9c',
+          height: '12px'
+    });
+    $('#php1').LineProgressbar({
+          percentage: 92,
+          fillBackgroundColor: 'red',
+          height: '12px'
+    });
+    $('#php2').LineProgressbar({
+          percentage: 95,
+          fillBackgroundColor: 'green',
+          height: '12px'
+    });
+    $('#php3').LineProgressbar({
+          percentage: 90,
+          fillBackgroundColor: 'blue',
+          height: '12px'
+    });
+=======
+>>>>>>> beffc04b8cd7deb4c7f1b89d40ccf133c712aecb
 });
